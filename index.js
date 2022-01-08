f21f0e7eedffa24794b022a14db5dc3423021fd0
fareddru@gmail.com <71222193+Faredpy@users.noreply.github.com>
2022-01-08 20:14:46 +0300

    Release 0.3

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ require('dotenv').config()
 const {Sequelize} = require('sequelize')
 const sequelize = require('./models/config/config')
 const cors = require('cors')
+const router = require('./routes/indexRouter')
+const morgan = require('morgan')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 
 const PORT = process.env.PORT || 3002
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use('/api', router)
+app.use(morgan('dev'))
+
+// Middleware который работает с ошибкой, обязательное регистрируется в самом конце! (замыкающий и без next)
+app.use(errorHandler)
 
 
 const start = async () => {
